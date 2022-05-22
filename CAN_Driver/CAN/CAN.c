96ffe08fe738c0f60f3dff228385af98cc439f2e
Kunal Salvi <kunalsalvi63@gmail.com>
2022-05-22 15:35:39 +0530

    Updated CAN Driver's source file by adding timestamp capabilities

--- CAN_Driver/CAN/CAN.c
+++ CAN_Driver/CAN/CAN.c
@@ -9,7 +9,7 @@
 #include "CAN.h"
 
 
-void CAN_Init(int32_t baudrate)
+void CAN_Init(CAN_Mailbox mailbox)
 {
 	RCC -> APB1ENR &= ~RCC_APB1ENR_CAN1EN;
 	GPIO_Pin_Setup(GPIOA, 12, ALT_PUSH_PULL_OUTPUT);
@@ -21,16 +21,69 @@ void CAN_Init(int32_t baudrate)
 	CAN1 -> MCR |= CAN_MCR_INRQ;
 	while((CAN1 -> MSR & CAN_MSR_SLAK)){}
 	while(!(CAN1-> MSR & CAN_MSR_INAK)){}
-	CAN1 -> BTR = baudrate;
+	CAN1 -> BTR = mailbox.baudrate;
 	CAN1->BTR |= CAN_BTR_LBKM;  //SET LBKM BIT
+
+	if(mailbox.timestamp == CAN_Timestamp_Enable)
+	{
+		CAN1 -> MCR |= CAN_MCR_TTCM;
+	}
+	else
+	{
+		CAN1 -> MCR &= ~CAN_MCR_TTCM;
+	}
+
     CAN1->MCR &= ~CAN_MCR_INRQ;
     CAN1->MCR &= ~CAN_MCR_INRQ;
     while((CAN1->MSR & CAN_MSR_INAK));
 }
 
-
-void CAN_Enter_Sleep_Mode(bool enable_disable)
+/*
+ * Figure out which mode is the controller is in right now.
+ * Print that out in debug mode
+ * Then make a decision to flip the necessary bits to go into Sleep mode from that mode.
+ */
+void CAN_Enter_Sleep_Mode(void)
 {
+	switch (CAN_Get_Operating_Mode())
+	{
+//=================================================================================================//
+		case CAN_Initialization_Mode:
+		{
+#if _CAN_DEBUG_
+			printConsole("Controller is going from Initialization Mode to Sleep Mode \r\n");
+#endif
+			CAN1 -> MCR |= CAN_MCR_SLEEP;
+			CAN1 -> MCR &= ~CAN_MCR_INRQ;
+			while(!(CAN1 -> MSR & CAN_MSR_SLAK)){} //Wait till SLAK does not go to 1
+			while((CAN1 -> MSR & CAN_MSR_INAK)){}  //Wait till INAK does not go to 0
+#if _CAN_DEBUG_
+			printConsole("Controller is in Sleep Mode \r\n");
+#endif
+		}
+			break;
+//=================================================================================================//
+		case CAN_Normal_Mode:
+		{
+#if _CAN_DEBUG_
+			printConsole("Controller is going from Normal Mode to Sleep Mode \r\n");
+#endif
+			CAN1 -> MCR |= CAN_MCR_SLEEP;
+			while(!(CAN1 -> MSR & CAN_MSR_SLAK)){} //Wait till SLAK does not go to 1
+#if _CAN_DEBUG_
+			printConsole("Controller is in Sleep Mode \r\n");
+#endif
+		}
+			break;
+//=================================================================================================//
+		case CAN_Sleep_Mode:
+		{
+#if _CAN_DEBUG_
+			printConsole("Controller is in Sleep Mode \r\n");
+#endif
+		}
+			break;
+	}
 
 }
 
@@ -83,6 +136,11 @@ void CAN_ID_Filter(int filter_number, int filter_type, int32_t reg1, int32_t reg
 
 void CAN_Send_Payload(CAN_Mailbox mailbox)
 {
+
+	CAN1 -> sTxMailBox[0].TDHR = 0x00;
+	CAN1 -> sTxMailBox[0].TDLR = 0x00;
+
+
 	switch (mailbox.mailbox_id)
 	{
 //=================================================================================================//
@@ -97,7 +155,7 @@ void CAN_Send_Payload(CAN_Mailbox mailbox)
 					break;
 				case CAN_ID_Extended:
 				{
-					CAN1 -> sTxMailBox[0].TIR  = mailbox.ID << 2;
+					CAN1 -> sTxMailBox[0].TIR  = mailbox.ID << 3;
 					CAN1 -> sTxMailBox[0].TIR  |= CAN_TI0R_EXID;
 				}
 					break;
@@ -116,9 +174,9 @@ void CAN_Send_Payload(CAN_Mailbox mailbox)
 				}
 					break;
 			}
-			CAN1 -> sTxMailBox[0].TDTR = mailbox.data_length;
-			CAN1 -> sTxMailBox[0].TDHR = mailbox.data[7] << 24 | mailbox.data[6] << 16 | mailbox.data[5] << 8 | mailbox.data[4] << 0;
-			CAN1 -> sTxMailBox[0].TDLR = mailbox.data[3] << 24 | mailbox.data[2] << 16 | mailbox.data[1] << 8 | mailbox.data[0] << 0;
+			CAN1 -> sTxMailBox[0].TDTR = mailbox.data_length | CAN_TDT0R_TGT;
+//			CAN1 -> sTxMailBox[0].TDHR = mailbox.data[7] << 24 | mailbox.data[6] << 16 | mailbox.data[5] << 8 | mailbox.data[4] << 0;
+//			CAN1 -> sTxMailBox[0].TDLR = mailbox.data[3] << 24 | mailbox.data[2] << 16 | mailbox.data[1] << 8 | mailbox.data[0] << 0;
 			CAN1 -> sTxMailBox[0].TIR  |= CAN_TI0R_TXRQ;
 		}
 			break;
@@ -135,7 +193,7 @@ void CAN_Send_Payload(CAN_Mailbox mailbox)
 					break;
 				case CAN_ID_Extended:
 				{
-					CAN1 -> sTxMailBox[1].TIR  = mailbox.ID << 2;
+					CAN1 -> sTxMailBox[1].TIR  = mailbox.ID << 3;
 					CAN1 -> sTxMailBox[1].TIR  |= CAN_TI1R_EXID;
 				}
 					break;
@@ -173,7 +231,7 @@ void CAN_Send_Payload(CAN_Mailbox mailbox)
 							break;
 						case CAN_ID_Extended:
 						{
-							CAN1 -> sTxMailBox[2].TIR  = mailbox.ID << 2;
+							CAN1 -> sTxMailBox[2].TIR  = mailbox.ID << 3;
 							CAN1 -> sTxMailBox[2].TIR  |= CAN_TI2R_EXID;
 						}
 							break;
